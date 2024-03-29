6538cbfe8febfb546fb87aea548da4c60567032b
Ammar Farhan Mohamad Rizam <ammar_22006911@utp.edu.my>
2024-03-29 13:14:36 +0800

    Add about route from root

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,22 @@
 <script>
-    let title = "Ninja Gaming Guides";
-
-    const updateTitle = () => {
-        title = "Something Completely Different";
-    };
 </script>
 
 <div class="index">
-    <h2>{title}</h2>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed adipisci doloribus, nobis similique, provident dignissimos, fugit cumque dicta iure temporibus rem. Eaque reiciendis corrupti ut voluptas repudiandae consectetur accusamus cumque, nesciunt labore libero esse. Cupiditate temporibus autem facilis expedita error sunt nemo inventore nisi dolores quaerat. Quam nam ut fugit consectetur incidunt sint ipsum culpa corporis! Necessitatibus amet vitae accusantium.</p>
-
-    <button on:click={updateTitle}>Change Title</button>
-    <input type="text" bind:value={title}>
+	<h2>Welcome</h2>
+	<p>
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed adipisci doloribus, nobis
+		similique, provident dignissimos, fugit cumque dicta iure temporibus rem. Eaque reiciendis
+		corrupti ut voluptas repudiandae consectetur accusamus cumque, nesciunt labore libero esse.
+		Cupiditate temporibus autem facilis expedita error sunt nemo inventore nisi dolores quaerat.
+		Quam nam ut fugit consectetur incidunt sint ipsum culpa corporis! Necessitatibus amet vitae
+		accusantium.
+	</p>
 </div>
 
 <style>
-    .index {
-        text-align: center;
-        display: block;
-        margin: 20px auto;
-    }
+	.index {
+		text-align: center;
+		display: block;
+		margin: 20px auto;
+	}
 </style>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,19 @@
+<div class="about">
+	<h2>About Us</h2>
+	<p>
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus sit
+		minus maiores consequatur incidunt sapiente officia illo eum optio, dicta
+		reiciendis iste voluptatum aliquid, quas delectus, quae sed voluptatem.
+		Porro architecto necessitatibus tenetur alias saepe animi, quasi ea magnam,
+		officiis dolor molestias sunt quam fuga eveniet id in quae officia quidem
+		hic culpa nobis iusto dolores omnis. Ipsam, incidunt cupiditate!
+	</p>
+</div>
+
+<style>
+	.about {
+		text-align: center;
+		display: block;
+		margin: 20px auto;
+	}
+</style>
