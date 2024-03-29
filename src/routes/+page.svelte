ab71edad012b157b3ed65c665fa71ebedd8ea44c
Ammar Farhan Mohamad Rizam <ammar_22006911@utp.edu.my>
2024-03-29 12:45:16 +0800

    Review Svelte reactive components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    let title = "Ninja Gaming Guides";
+
+    const updateTitle = () => {
+        title = "Something Completely Different";
+    };
+</script>
+
+<div class="index">
+    <h2>{title}</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed adipisci doloribus, nobis similique, provident dignissimos, fugit cumque dicta iure temporibus rem. Eaque reiciendis corrupti ut voluptas repudiandae consectetur accusamus cumque, nesciunt labore libero esse. Cupiditate temporibus autem facilis expedita error sunt nemo inventore nisi dolores quaerat. Quam nam ut fugit consectetur incidunt sint ipsum culpa corporis! Necessitatibus amet vitae accusantium.</p>
+
+    <button on:click={updateTitle}>Change Title</button>
+    <input type="text" bind:value={title}>
+</div>
+
+<style>
+    .index {
+        text-align: center;
+        display: block;
+        margin: 20px auto;
+    }
+</style>
