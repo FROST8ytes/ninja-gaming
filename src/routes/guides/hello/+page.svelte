e102385a5648472b68339ab3afdd3fbd59bc9031
Ammar Farhan Mohamad Rizam <ammar_22006911@utp.edu.my>
2024-03-29 13:21:11 +0800

    Add nested routing

---
+++ src/routes/guides/hello/+page.svelte
@@ -0,0 +1,22 @@
+<div class="hello">
+	<h2>Hello from Guides</h2>
+	<p>
+		Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda architecto doloribus sint
+		consequuntur? Laboriosam nulla itaque ex, unde nobis suscipit distinctio accusantium dolores
+		mollitia recusandae asperiores at iste minima alias saepe! Laudantium repellat impedit, nesciunt
+		placeat quae minima mollitia dolore amet velit dolores! Facere fugiat veniam quasi consectetur
+		voluptas assumenda eaque dignissimos ullam, voluptates reprehenderit sit, accusantium aliquid
+		debitis vel voluptatibus dolore. Aliquid dolore rerum blanditiis quae debitis nemo repudiandae
+		alias in sit at fuga voluptas porro quos voluptatem, aperiam voluptatibus autem aspernatur
+		pariatur. Sequi eos unde doloremque sed facere tempore dolorem aspernatur laborum, quos deleniti
+		itaque nihil, doloribus architecto!
+	</p>
+</div>
+
+<style>
+	.hello {
+		text-align: center;
+		display: block;
+		margin: 20px auto;
+	}
+</style>
